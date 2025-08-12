2d6d13b75b4e7249f201713cb97a6ae216d6ce2b
unknown <obiangjeremias@gmail.com>
2025-08-12 19:33:09 +0100

    Use environment variables for SMTP credentials

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,36 +11,14 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Mailtrap transporter setup
-const mailtrapTransporter = nodemailer.createTransport({
-  host: process.env.MAILTRAP_HOST,
-  port: process.env.MAILTRAP_PORT,
-  auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS
-  }
-});
-
-// Gmail transporter setup
-const gmailTransporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'obiangjeremias@gmail.com', // your Gmail address
-    pass: 'ipqecqymwcnbcqoy'    // your Gmail App Password
-  },
-  tls: {
-    rejectUnauthorized: false
-  }
-});
-
 // Corporate email transporter setup
 const transporter = nodemailer.createTransport({
   host: 'smtp.panel247.com',
   port: 465, // try 587 if you get connection errors
   secure: true, // true for port 465, false for 587
   auth: {
-    user: 'info@thomsangeg.com',
-    pass: 'Malabo@2023'
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   },
   tls: {
     rejectUnauthorized: false
@@ -63,11 +39,10 @@ transporter.verify(function(error, success) {
 app.post('/send-email', async (req, res) => {
   const { user_name, user_email,user_phone, user_service_requested, user_message } = req.body;
   
-
   try {
     await transporter.sendMail({
-      from: 'info@thomsangeg.com', // must match SMTP user
-      to: 'info@thomsangeg.com', // or your desired recipient
+      from: process.env.SMTP_USER, // must match SMTP user
+      to: process.env.SMTP_USER, // or your desired recipient
       replyTo: user_email, // client's email for reply
       subject: user_service_requested || 'New Contact Form Message',
       text: `Name: ${user_name}\nEmail: ${user_email}\nPhone: ${user_phone}\nService: ${user_service_requested}\nMessage: ${user_message}`
